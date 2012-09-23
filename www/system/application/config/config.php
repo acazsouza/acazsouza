f6d7e987a7434867c42244edfa0f1b433b6e0d5c
Acaz <acazsouza@gmail.com>
2012-09-22 22:03:35 -0300

    xml feed in feed tag html

--- www/system/application/config/config.php
+++ www/system/application/config/config.php
@@ -11,7 +11,7 @@
 |	http://example.com/
 |
 */
-$config['base_url']	= "http://acazsouza.localhost/";
+$config['base_url']	= "http://acazsouza.com.br/";
 
 /*
 |--------------------------------------------------------------------------
@@ -66,7 +66,7 @@ $config['url_suffix'] = "";
 | than english.
 |
 */
-$config['language']	= "english";
+$config['language']	= "portuguese";
 
 /*
 |--------------------------------------------------------------------------
--- www/system/application/views/master_page.php
+++ www/system/application/views/master_page.php
@@ -1,10 +1,10 @@
 <!doctype html>
-<html lang="en">
+<html lang="pt-br">
     <head>
         <meta charset="utf-8">
         <title><?php echo $title == '' ? '' : $title . ' | '  ?>acazsouza.com.br</title>
         <link rel="stylesheet" href="/resources/css/style.css">
-        <link rel="alternate" type="application/rss+xml" title="acazsouza.com.br Feed" href="/feed" />
+        <link rel="alternate" type="application/rss+xml" title="acazsouza.com.br Feed" href="http://www.acazsouza.com.br/feed" />
     </head>
     <body>
     	<div id="header">
@@ -17,8 +17,8 @@
                 <a href="http://www.delicious.com/acazsouza" title="Abre uma nova janela" target="_blank">Delicious</a> |
                 <a href="https://github.com/acazsouza" title="Abre uma nova janela" target="_blank">GitHub</a> |
                 <a href="http://www.quora.com/Acaz-Pereira" title="Abre uma nova janela" target="_blank">Quora</a> | 
-                <a href="/feed" title="Abre uma nova janela" target="_blank">Feed</a>
-                <span>ou entre em contato em: acazsouza@gmail.com</span>
+                <a href="http://feeds.feedburner.com/acazsouza" title="Abre uma nova janela" target="_blank">Feed</a>
+                <span>ou entre em contato em: acazsouza<x>@</x>gmail.com</span>
             </div>
         </div>
         <div id="content">
